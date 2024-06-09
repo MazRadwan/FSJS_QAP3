e463cb734bc2ccf75416cec791a58a8cc11098b0
Maz Radwan <maz.radwan@keyin.com>
2024-06-09 05:58:03 -0230

    home page complete

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 const server = require("./server");
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+// server.listen(3000, () => {
+// //   console.log("Server running on port 3000");
+// });
--- server.js
+++ server.js
@@ -1,15 +1,59 @@
 const http = require("http");
 const path = require("path");
+const fs = require("fs");
 const routes = require("./routes");
 const myEmitter = require("./logEvents");
 const handleError = require("./errorHandler");
 
 global.DEBUG = true;
 
+// Helper function to serve static files
+const serveStaticFile = (filePath, res) => {
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      handleError(err, res);
+      return;
+    }
+
+    const extname = path.extname(filePath);
+    let contentType = "text/html";
+
+    switch (extname) {
+      case ".js":
+        contentType = "text/javascript";
+        break;
+      case ".css":
+        contentType = "text/css";
+        break;
+      case ".png":
+        contentType = "image/png";
+        break;
+    }
+
+    res.writeHead(200, { "Content-Type": contentType });
+    res.end(content, "utf-8");
+  });
+};
+
 const server = http.createServer((req, res) => {
   if (DEBUG) console.log("Request Url:", req.url);
   let filePath = path.join(__dirname, "views");
   let routeHandler;
+
+  // Serve static files from root, /assets, /styles, /scripts
+  if (req.url === "/index.js") {
+    serveStaticFile(path.join(__dirname, req.url), res);
+    return;
+  } else if (req.url.startsWith("/assets")) {
+    serveStaticFile(path.join(__dirname, req.url), res);
+    return;
+  } else if (req.url.startsWith("/styles")) {
+    serveStaticFile(path.join(__dirname, req.url), res);
+    return;
+  } else if (req.url.startsWith("/scripts")) {
+    serveStaticFile(path.join(__dirname, req.url), res);
+    return;
+  }
 
   switch (req.url) {
     case "/":
@@ -41,6 +85,10 @@ const server = http.createServer((req, res) => {
 
   routeHandler(filePath, res);
   myEmitter.emit("routeAccessed", req.url);
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
 
 module.exports = server;
